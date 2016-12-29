889226b24e2d92af44cd364f3c084a24b97f0b3a
Wei-ju Wu <weiju.wu@gmail.com>
2016-12-29 12:36:57 -0800

    omit the egrin2_api.py file from coverage

--- bin/run_coverage.sh
+++ bin/run_coverage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PYTHONPATH=src EGRIN2API_SETTINGS=settings.cfg coverage run test/api_test.py xml && coverage xml --include=src/*
+PYTHONPATH=src EGRIN2API_SETTINGS=settings.cfg coverage run test/api_test.py xml && coverage xml --include=src/app.py
